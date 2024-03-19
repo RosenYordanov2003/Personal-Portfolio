2d9a385d068895fb2e32234aed622f071554e452
Rosen Yordanov <roskata182003@gmail.com>
2024-03-19 18:30:03 +0200

    Add Navigation component with Navigation.css file

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,17 @@
+import "../Navigation/Navigation.css";
+
+export default function Navigation(){
+    return(
+      <header className="nav-container">
+        <nav>
+            <ul className="navigation">
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Education</li>
+            </ul>
+        </nav>
+       <a className="github-button" href="https://github.com/RosenYordanov2003">Github</a>
+      </header>
+    )
+}
